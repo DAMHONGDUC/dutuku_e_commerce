c553ab860e3eaee747174ce836c67fac92065179
Dam Hong Duc <hongduc001h@gmail.com>
2025-06-28 16:28:42 +0700

    feat: config home app bar scroll able

--- lib/src/presentation/home/home_screen.dart
+++ lib/src/presentation/home/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:dutuku_e_commerce/src/core/core.dart';
-import 'package:dutuku_e_commerce/src/presentation/home/components/home_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:system_design_flutter/index.dart';
 
 import 'banner_carousel_section/banner_carousel_section.dart';
 import 'categories_section/categories_section.dart';
+import 'components/home_app_bar.dart';
 import 'recommend_products_section/recommend_products_section.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,28 +14,39 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: context.colorTheme.pageDefault,
-      appBar: HomeAppBar(),
-      body: Padding(
-        padding: EdgeInsets.all(SdSpacing.s8),
-        child: SingleChildScrollView(
+      body: SdScrollConfig(
+        child: CustomScrollView(
           clipBehavior: Clip.none,
-          child: Column(
-            children: [
-              // banner section
-              const BannerCarouselSection(),
-              const SdVerticalSpacing(),
+          slivers: [
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  const HomeAppBar(),
 
-              // categories section
-              const SectionHeader(title: 'Categories'),
-              const CategoriesSection(),
-              const SdVerticalSpacing(),
+                  Container(
+                    padding: EdgeInsets.all(SdSpacing.s8),
+                    child: Column(
+                      children: [
+                        // banner section
+                        const BannerCarouselSection(),
+                        const SdVerticalSpacing(),
 
-              // recommend product section
-              const SectionHeader(title: 'New Arrivals 🔥'),
-              const RecommendProductsSection(),
-              const SdVerticalSpacing(),
-            ],
-          ),
+                        // categories section
+                        const SectionHeader(title: 'Categories'),
+                        const CategoriesSection(),
+                        const SdVerticalSpacing(),
+
+                        // recommend product section
+                        const SectionHeader(title: 'New Arrivals 🔥'),
+                        const RecommendProductsSection(),
+                        const SdVerticalSpacing(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
