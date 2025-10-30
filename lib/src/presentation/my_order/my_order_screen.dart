2f8c44642b2e14b45bb085ce316ffb66888e99f2
Dam Hong Duc <hongduc001h@gmail.com>
2025-10-30 22:31:03 +0700

    feat: add my order app bar

--- lib/src/presentation/my_order/my_order_screen.dart
+++ lib/src/presentation/my_order/my_order_screen.dart
@@ -2,13 +2,10 @@ import 'package:dutuku_e_commerce/src/core/core.dart';
 import 'package:dutuku_e_commerce/src/di/injector.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:ionicons/ionicons.dart';
 import 'package:system_design_flutter/index.dart';
 
+import 'components/my_order_app_bar.dart';
 import 'my_order_controller.dart';
-
-final _kIconPadding = SdSpacing.s6;
-final _kTitleSpacing = SdSpacing.s12;
 
 class MyOrderScreen extends StatelessWidget {
   const MyOrderScreen({super.key});
@@ -54,54 +51,7 @@ class __MyOrderViewState extends State<_MyOrderView>
   Widget build(BuildContext context) {
     return SdSafeAreaScaffold(
       backgroundColor: context.colorTheme.surfaceDefault,
-      appBar: SdAppBar(
-        titleText: 'My order',
-        showBackButton: false,
-        titleSpacing: _kTitleSpacing,
-        actionPadding: EdgeInsets.only(right: _kTitleSpacing - _kIconPadding),
-        actions: [
-          SdIconButton(
-            padding: EdgeInsets.all(_kIconPadding),
-            onPressed: () {},
-            icon: SdIcon(
-              iconData: Ionicons.search,
-              color: context.colorTheme.primary,
-            ),
-          ),
-          SdHorizontalSpacing(xRatio: 0.5),
-          SdIconButton(
-            padding: EdgeInsets.all(_kIconPadding),
-            onPressed: () {},
-            icon: SdIcon(
-              iconData: Ionicons.chatbubble_ellipses_outline,
-              color: context.colorTheme.primary,
-            ),
-          ),
-        ],
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(SdSpacing.s40),
-          child: TabBar(
-            tabAlignment: TabAlignment.start,
-            isScrollable: true,
-            controller: _tabController,
-            dividerColor: SdColors.transparent,
-            padding: EdgeInsets.zero,
-            labelPadding: EdgeInsets.symmetric(
-              horizontal: SdSpacing.s16,
-              vertical: SdSpacing.s2,
-            ),
-            labelStyle: SdTextStyle.body14()
-                .wColor(context.colorTheme.primary)
-                .wSemiBold(),
-            unselectedLabelStyle: SdTextStyle.body14().wColor(
-              context.colorTheme.textDefault,
-            ),
-            tabs: OrderStatusExt.allStatus
-                .map((e) => Tab(text: e.label, height: SdSpacing.s32))
-                .toList(),
-          ),
-        ),
-      ),
+      appBar: MyOrderAppBar(controller: _tabController),
       child: Column(children: []),
     );
   }
