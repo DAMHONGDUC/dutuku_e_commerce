a35b0c678250576d282f28d3dc57d0838f3b569d
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:13 +0700

    feat(core): add exception and failure types

---
+++ lib/src/core/exceptions/cache_exception.dart
@@ -0,0 +1,4 @@
+class CacheException implements Exception {
+  const CacheException([this.message]);
+  final String? message;
+}
---
+++ lib/src/core/failures/config_failures.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  const Failure();
+}
+
+class CacheFailure extends Failure {
+  @override
+  List<Object> get props => [];
+}
+
+class ServerFailure extends Failure {
+  @override
+  List<Object> get props => [];
+}
+
+class NetworkFailure extends Failure {
+  @override
+  List<Object> get props => [];
+}
+
+class ValidationFailure extends Failure {
+  final String message;
+
+  const ValidationFailure(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
