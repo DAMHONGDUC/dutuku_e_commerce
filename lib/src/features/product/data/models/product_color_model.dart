75703e587b481739beeeb868f2bf49552395a141
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:14 +0700

    feat(product): add core data models

---
+++ lib/src/features/product/data/models/product_color_model.dart
@@ -0,0 +1,7 @@
+class ProductColorModel {
+  final String? colorName;
+  final String? imageUrl;
+  final String? colorCode;
+
+  const ProductColorModel({this.colorName, this.imageUrl, this.colorCode});
+}
