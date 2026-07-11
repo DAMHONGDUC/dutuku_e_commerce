6b6ddbb70a3052a3c0a83a43e0eff8a6e0b87e5f
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:14 +0700

    feat: product - add related products section

---
+++ lib/src/features/product/presentation/product_detail/related_product_section/related_products_controller.dart
@@ -0,0 +1,29 @@
+import 'package:dutuku_e_commerce/src/features/product/domain/domain.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'related_products_state.dart';
+
+class RelatedProductsController extends Cubit<RelatedProductsState> {
+  final GetRelatedProductUsecase _getRelatedProductUsecase;
+
+  RelatedProductsController(this._getRelatedProductUsecase)
+    : super(RelatedProductsInitial());
+
+  Future<void> getData({required String productId}) async {
+    emit(RelatedProductsLoadingState());
+
+    final result = await _getRelatedProductUsecase.call(
+      GetRelatedProductParams(productId: productId, limit: 6),
+    );
+
+    result.fold(
+      (failure) {
+        emit(RelatedProductsErrorState(errorMsg: ''));
+      },
+      (r) {
+        emit(RelatedProductsLoadedState(products: r));
+      },
+    );
+  }
+}
