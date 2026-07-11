da4fa3830d6c1d7a40d805ee1d18c640f40bd166
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:13 +0700

    feat(notification): add repository contract and usecase

---
+++ lib/src/features/notification/domain/domain.dart
@@ -0,0 +1,5 @@
+export 'entities/notification_entity.dart';
+export 'entities/notification_category_entity.dart';
+export 'entities/notification_data.dart';
+export 'repositories/notification_repository.dart';
+export 'usecases/get_notifications_usecase.dart';
