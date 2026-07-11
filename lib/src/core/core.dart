d6b7d37100f324eaa5fa43aeb3cb28a35de2d543
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:13 +0700

    feat: core - add core barrel export

---
+++ lib/src/core/core.dart
@@ -0,0 +1,34 @@
+// Config
+export 'navigation/router_config.dart';
+
+// Constants
+export './constants/constants.dart';
+
+// Resources
+export './resources/resources.dart';
+
+// l10n
+export './l10n/l10n.dart';
+
+// enums
+export './enums/enums.dart';
+
+// widgets
+export './widgets/widgets.dart';
+
+// failures
+export 'failures/config_failures.dart';
+
+// usecase
+export './usecase/usecase.dart';
+
+// emv
+export './env/app_env.dart';
+
+// exceptions
+export './exceptions/cache_exception.dart';
+export './exceptions/network_exception.dart';
+export './exceptions/server_exception.dart';
+
+// utils
+export './utils/utils.dart';
