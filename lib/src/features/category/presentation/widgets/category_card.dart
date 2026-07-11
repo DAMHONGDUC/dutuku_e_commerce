f97d6758e5c66c23975dd16844b70be62f2d57a0
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:13 +0700

    feat: category - add category card widgets

---
+++ lib/src/features/category/presentation/widgets/category_card.dart
@@ -0,0 +1,37 @@
+import 'package:dutuku_e_commerce/src/core/core.dart';
+import 'package:dutuku_e_commerce/src/features/category/domain/domain.dart';
+import 'package:flutter/material.dart';
+import 'package:system_design_flutter/index.dart';
+
+class CategoryCard extends StatelessWidget {
+  final CategoryEntity category;
+
+  const CategoryCard({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: UIConstants.categoriesCardWidth,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SdImage(
+            imagePath: category.imageUrl,
+            width: UIConstants.categoriesImageWidth,
+            height: UIConstants.categoriesImageHeight,
+            borderRadiusValue: SdSpacing.s100,
+          ),
+          SdVerticalSpacing(xRatio: 0.5),
+          Text(
+            category.name,
+            style: context.textTheme.heading14,
+            textAlign: TextAlign.center,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ],
+      ),
+    );
+  }
+}
