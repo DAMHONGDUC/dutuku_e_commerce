bad76e37728a4efb8a7ada25022b42d9a1a77597
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:14 +0700

    feat(product): add product detail sections

---
+++ lib/src/features/product/presentation/product_detail/components/product_info_skeleton.dart
@@ -0,0 +1,51 @@
+import 'package:dutuku_e_commerce/src/core/core.dart';
+import 'package:dutuku_e_commerce/src/features/product/presentation/widgets/product_grid_skeleton.dart';
+import 'package:flutter/material.dart';
+import 'package:system_design_flutter/index.dart';
+
+class ProductInfoSkeleton extends StatelessWidget {
+  const ProductInfoSkeleton({super.key, required this.imgHeight});
+  final double imgHeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SdSkeleton(height: imgHeight, borderRadius: BorderRadius.zero),
+          Container(
+            color: context.colorTheme.surfaceDefault,
+            padding: EdgeInsets.all(SdSpacing.s16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // skeleton 1
+                SdVerticalSpacing(),
+                SdSkeleton(width: 300),
+                SdVerticalSpacing(),
+                SdSkeleton(width: 100),
+
+                // skeleton 2
+                SdVerticalSpacing(xRatio: 2),
+                SdSkeleton(height: 80),
+
+                // skeleton 3
+                SdVerticalSpacing(xRatio: 2),
+                SdSkeleton(),
+                SdVerticalSpacing(),
+                SdSkeleton(),
+                SdVerticalSpacing(),
+                SdSkeleton(),
+
+                // skeleton 5
+                SdVerticalSpacing(xRatio: 2),
+                ProductGridSkeleton(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
