5738cef58ca8ad8be192c977db2b55a7476bf84c
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:13 +0700

    feat(profile): add profile screen, routes and DI

---
+++ lib/src/features/profile/presentation/profile/profile_screen.dart
@@ -0,0 +1,76 @@
+import 'package:dutuku_e_commerce/src/core/core.dart';
+import 'package:dutuku_e_commerce/src/di/injector.dart';
+import 'package:dutuku_e_commerce/src/features/profile/presentation/profile/components/setting_section.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:system_design_flutter/index.dart';
+
+import 'components/profile_app_bar.dart';
+import 'components/profile_skeleton.dart';
+import 'profile_controller.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => getIt<ProfileController>()..getData()),
+      ],
+      child: _ProfileView(),
+    );
+  }
+}
+
+class _ProfileView extends StatelessWidget {
+  const _ProfileView();
+
+  Future _onRefresh(BuildContext context) async {
+    context.read<ProfileController>().getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SdSafeAreaScaffold(
+      backgroundColor: context.colorTheme.pageDefault,
+      contentBgColor: context.colorTheme.pageDefault,
+      appBar: ProfileAppBar(),
+      child: BlocConsumer<ProfileController, ProfileState>(
+        listener: (context, state) {
+          // TODO: implement listener
+        },
+        buildWhen: (previous, current) {
+          return current is ProfileLoadingState ||
+              current is ProfileLoadedState ||
+              current is ProfileErrorState;
+        },
+        builder: (context, state) {
+          if (state is ProfileLoadingState) {
+            return ProfileSkeleton();
+          } else if (state is ProfileErrorState) {
+            return EmptyView(message: state.errorMsg);
+          } else if (state is ProfileLoadedState) {
+            return RefreshWrapper(
+              onRefresh: () => _onRefresh(context),
+              child: SdListView(
+                padding: EdgeInsets.all(SdSpacing.s16),
+                items: state.listSettingSection,
+                separatorBuilder: (context, index) {
+                  return SdVerticalSpacing();
+                },
+                itemBuilder: (_, index) {
+                  return SettingSection(
+                    settingSection: state.listSettingSection[index],
+                  );
+                },
+              ),
+            );
+          }
+
+          return SizedBox.shrink();
+        },
+      ),
+    );
+  }
+}
---
+++ lib/src/features/profile/presentation/profile/profile_state.dart
@@ -0,0 +1,28 @@
+part of 'profile_controller.dart';
+
+sealed class ProfileState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class ProfileInitial extends ProfileState {}
+
+class ProfileLoadingState extends ProfileState {}
+
+class ProfileLoadedState extends ProfileState {
+  final List<SettingSectionEntity> listSettingSection;
+
+  ProfileLoadedState({required this.listSettingSection});
+
+  @override
+  List<Object?> get props => [listSettingSection];
+}
+
+class ProfileErrorState extends ProfileState {
+  final String errorMsg;
+
+  ProfileErrorState({required this.errorMsg});
+
+  @override
+  List<Object?> get props => [errorMsg];
+}
