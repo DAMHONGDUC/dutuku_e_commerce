e89d466d77b5ecdadb797fa866b65370954c101a
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:13 +0700

    feat: core - add router and navigation config

---
+++ lib/src/core/navigation/router_config.dart
@@ -0,0 +1,39 @@
+import 'package:dutuku_e_commerce/src/core/navigation/extra_codec_config.dart';
+import 'package:dutuku_e_commerce/src/features/bottom_tab/bottom_tab_stack.dart';
+import 'package:dutuku_e_commerce/src/features/login/config/login_stack.dart';
+import 'package:dutuku_e_commerce/src/features/product/presentation/product_detail/config/product_stack.dart';
+import 'package:dutuku_e_commerce/src/features/register/config/register_stack.dart';
+import 'package:dutuku_e_commerce/src/features/splash/config/splash_routes.dart';
+import 'package:dutuku_e_commerce/src/features/splash/config/splash_stack.dart';
+import 'package:dutuku_e_commerce/src/features/tutorial/config/tutorial_stack.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final GlobalKey<NavigatorState> kRootNavigatorKey = GlobalKey<NavigatorState>(
+  debugLabel: 'root',
+);
+
+final GlobalKey<NavigatorState> kHomeTabNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'homeTabNav');
+final GlobalKey<NavigatorState> kMyOrderTabNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'myOrderTabNav');
+final GlobalKey<NavigatorState> kFavoriteTabNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'favoriteTabNav');
+final GlobalKey<NavigatorState> kProfileTabNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'profileTabNav');
+
+final GoRouter kAppRouter = GoRouter(
+  observers: [],
+  navigatorKey: kRootNavigatorKey,
+  initialLocation: SplashRoutes.splash.path,
+  debugLogDiagnostics: true,
+  extraCodec: const ExtraCodecConfig(),
+  routes: [
+    ...SplashStack.routes,
+    ...TutorialStack.routes,
+    ...LoginStack.routes,
+    ...RegisterStack.routes,
+    ...ProductStack.routes,
+    BottomTabStack.statefulShellRoute,
+  ],
+);
