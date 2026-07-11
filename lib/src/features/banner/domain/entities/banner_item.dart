0457c52f9103d094dcff762b533e3a3aa0c1d0e2
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:13 +0700

    feat(banner): add domain layer

---
+++ lib/src/features/banner/domain/entities/banner_item.dart
@@ -0,0 +1,37 @@
+import 'package:dutuku_e_commerce/src/core/core.dart';
+
+import 'package:equatable/equatable.dart';
+
+class BannerItem extends Equatable {
+  final String imageUrl;
+  final String tag;
+  final BannerTagType tagType;
+  final String title;
+  final String subtitle;
+  final String price;
+  final String contentColor;
+  final int id;
+
+  const BannerItem({
+    required this.imageUrl,
+    required this.tag,
+    required this.tagType,
+    required this.title,
+    required this.subtitle,
+    required this.price,
+    required this.contentColor,
+    required this.id,
+  });
+
+  @override
+  List<Object?> get props => [
+    imageUrl,
+    tag,
+    tagType,
+    title,
+    subtitle,
+    price,
+    contentColor,
+    id,
+  ];
+}
