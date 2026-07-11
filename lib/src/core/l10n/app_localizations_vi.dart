1199f8ed38fd1f399120459a67640896cbfdef62
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:13 +0700

    feat: core - add localization resources (en, vi)

---
+++ lib/src/core/l10n/app_localizations_vi.dart
@@ -0,0 +1,16 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Vietnamese (`vi`).
+class AppLocalizationsVi extends AppLocalizations {
+  AppLocalizationsVi([String locale = 'vi']) : super(locale);
+
+  @override
+  String get app_name => 'Dutuku';
+
+  @override
+  String get splash_description => 'Any shopping just from home';
+}
