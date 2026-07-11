9d69043000faa14d8fa7a7c5fa5db9ed7cf71e47
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:14 +0700

    feat(home): add home screen, app bar and routes

---
+++ lib/src/features/home/presentation/components/home_sliver_app_bar.dart
@@ -0,0 +1,78 @@
+import 'package:dutuku_e_commerce/src/core/core.dart';
+import 'package:dutuku_e_commerce/src/features/home/presentation/config/home_routes.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:system_design_flutter/index.dart';
+
+final _kAppBarPadding = SdSpacing.s12;
+final _kAppBarPaddingTop = SdSpacing.s4;
+final _kAppBarPaddingBottom = SdSpacing.s8;
+final _kSearchBarHeight = SdSpacing.s40;
+final _kAppBarHeight =
+    _kSearchBarHeight + _kAppBarPaddingBottom + _kAppBarPaddingTop;
+final _kIconPadding = SdSpacing.s6;
+
+class HomeSliverAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const HomeSliverAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      // floating & snap => make app bar "snap" into view
+      floating: true,
+      snap: true,
+      pinned: false,
+      elevation: SdSpacing.s2,
+      shadowColor: context.colorTheme.primaryShadowDefault,
+      toolbarHeight: _kAppBarHeight,
+      backgroundColor: context.colorTheme.surfaceDefault,
+      surfaceTintColor: SdColors.transparent,
+      titleSpacing: 0,
+      title: Padding(
+        padding: EdgeInsets.only(
+          left: _kAppBarPadding,
+          right: _kAppBarPadding - _kIconPadding,
+          bottom: _kAppBarPaddingBottom,
+          top: _kAppBarPaddingTop,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(child: CustomSearchBar(height: _kSearchBarHeight)),
+            SdHorizontalSpacing(),
+            Row(
+              children: [
+                SdIconButton(
+                  padding: EdgeInsets.all(_kIconPadding),
+                  onPressed: () {
+                    GoRouter.of(context).push(HomeRoutes.search.fullPath);
+                  },
+                  icon: SdIcon(
+                    iconData: Ionicons.cart,
+                    color: context.colorTheme.primary,
+                  ),
+                ),
+                SdHorizontalSpacing(xRatio: 0.5),
+                SdIconButton(
+                  padding: EdgeInsets.all(_kIconPadding),
+                  onPressed: () {
+                    GoRouter.of(context).push(HomeRoutes.search.fullPath);
+                  },
+                  icon: SdIcon(
+                    iconData: Ionicons.chatbubble_ellipses_outline,
+                    color: context.colorTheme.primary,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      actions: null,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(_kAppBarHeight);
+}
