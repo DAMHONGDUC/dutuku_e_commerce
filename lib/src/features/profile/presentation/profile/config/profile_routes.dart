6e8d5a2bad563a6f182adb012f7cb5e936346183
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:13 +0700

    feat: profile - add profile screen, routes and DI

---
+++ lib/src/features/profile/presentation/profile/config/profile_routes.dart
@@ -0,0 +1,9 @@
+import 'package:system_design_flutter/config/router/sd_router.dart';
+
+class ProfileRoutes {
+  static const SdRouter profile = SdRouter(
+    name: 'profile',
+    path: '/profile',
+    fullPath: '/profile',
+  );
+}
