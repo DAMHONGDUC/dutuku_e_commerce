cae025adac274940e8c5eb524d0f80a73a6d8bd7
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:14 +0700

    feat(product): add product usecases

---
+++ lib/src/features/product/domain/usecases/get_product_detail_usecase.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:dutuku_e_commerce/src/core/core.dart';
+import 'package:dutuku_e_commerce/src/features/product/domain/domain.dart';
+
+class GetProductDetailUsecase implements UseCase<ProductEntity, String> {
+  final ProductRepository repository;
+
+  GetProductDetailUsecase(this.repository);
+
+  @override
+  Future<Either<Failure, ProductEntity>> call(String productId) async {
+    return await repository.getProductDetail(productId: productId);
+  }
+}
