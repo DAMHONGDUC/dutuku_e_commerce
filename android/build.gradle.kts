15a04d8ea4a6cbc74d87cd83297311fb450a22e2
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:11 +0700

    build: android - update Gradle configuration and wrapper

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+  id("com.google.gms.google-services") version "4.3.15" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -5,10 +9,7 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
