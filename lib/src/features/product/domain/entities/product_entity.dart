4124bdad86fabf064606f7c03df77e8b8b30b89c
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:14 +0700

    feat: product - add core product entities

---
+++ lib/src/features/product/domain/entities/product_entity.dart
@@ -0,0 +1,48 @@
+import 'package:dutuku_e_commerce/src/features/product/domain/entities/description.dart';
+import 'package:dutuku_e_commerce/src/features/product/domain/entities/product_color.dart';
+import 'package:dutuku_e_commerce/src/features/product/domain/entities/product_size.dart';
+import 'package:dutuku_e_commerce/src/features/product/domain/entities/review_comment.dart';
+import 'package:equatable/equatable.dart';
+
+class ProductEntity extends Equatable {
+  final String name;
+  final String brand;
+  final double price;
+  final String imageUrl;
+  final List<ProductColor> productColors;
+  final String id;
+  final int purchaserCount;
+  final Description? description;
+  final List<ReviewComment> reviewComments;
+  final double averageRating;
+  final List<ProductSize> productSizes;
+
+  const ProductEntity({
+    this.productColors = const [],
+    this.productSizes = const [],
+    required this.name,
+    required this.brand,
+    required this.price,
+    required this.imageUrl,
+    required this.id,
+    required this.purchaserCount,
+    this.description,
+    required this.reviewComments,
+    required this.averageRating,
+  });
+
+  @override
+  List<Object?> get props => [
+    name,
+    brand,
+    price,
+    imageUrl,
+    productColors,
+    id,
+    purchaserCount,
+    description,
+    reviewComments,
+    averageRating,
+    productSizes,
+  ];
+}
