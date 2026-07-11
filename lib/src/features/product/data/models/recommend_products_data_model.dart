f7911a33d3285cf52670350ce98efc291298c31a
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:14 +0700

    feat: product - add listing and review data models

---
+++ lib/src/features/product/data/models/recommend_products_data_model.dart
@@ -0,0 +1,10 @@
+import 'product_data_model.dart';
+
+class RecommendProductsDataModel extends ProductsDataModel {
+  const RecommendProductsDataModel({
+    super.items,
+    super.totalPage,
+    super.currentPage,
+    super.totalRecord,
+  });
+}
