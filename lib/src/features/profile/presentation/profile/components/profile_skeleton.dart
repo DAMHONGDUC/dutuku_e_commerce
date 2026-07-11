166ed9a5927246624c228d506728bf59ffaa5dc0
Dam Hong Duc <hongduc001h@gmail.com>
2026-07-11 20:13:13 +0700

    feat: profile - add profile UI components

---
+++ lib/src/features/profile/presentation/profile/components/profile_skeleton.dart
@@ -0,0 +1,21 @@
+import 'package:dutuku_e_commerce/src/core/core.dart';
+import 'package:flutter/material.dart';
+import 'package:system_design_flutter/index.dart';
+
+class ProfileSkeleton extends StatelessWidget {
+  const ProfileSkeleton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SdListView(
+      padding: EdgeInsets.all(SdSpacing.s16),
+      items: List.generate(10, (e) => e),
+      separatorBuilder: (context, index) {
+        return SdVerticalSpacing();
+      },
+      itemBuilder: (_, index) {
+        return ListTitleSkeleton(withAvatar: false, withTrailing: true);
+      },
+    );
+  }
+}
